315d675ba8172bdee0792dcc75e701837ef323cc
pawan <pavanyonex@gmail.com>
2023-06-25 22:44:29 +0530

    int removed from operator

--- calculator.py
+++ calculator.py
@@ -9,10 +9,7 @@ a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
 b=int(input("Enter number 2 : "))
 
-if a or b == 0:
-    print('invalid values')
-     
-elif o[0] in [ '+','-','*','/', '%','^']:
+if o[0] in [ '+','-','*','/', '%','^']:
     if o[0] == '+':
         out = a + b
     elif o[0] == '-':
@@ -21,10 +18,6 @@ elif o[0] in [ '+','-','*','/', '%','^']:
         out = a * b
     elif o[0] == '/':
         out = a//b
-    elif o[0] == '%':
-        out = a%b
-    elif o[0] == '^':
-        out = pow(a,b)
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
