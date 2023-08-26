f22dcd099cc1f73b4561650838bb392269fffabd
Akshansh Tripathi ! <vptripathi432@gmail.com>
2023-08-26 13:42:15 +0530

    the divide by zero is solved

--- calculator.py
+++ calculator.py
@@ -5,10 +5,10 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
-o=input("Enter operator : ")
-b=input("Enter number 2 : ")
-
+a=int(input("Enter number 1 : "))
+o=(input("Enter operator : "))
+b=int(input("Enter number 2 : "))
+out=0
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
         out = a + b
@@ -17,7 +17,10 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
+        if b==0:
+            print("error: divide by zero isnt possible.")
+        else:
+            out = a//b
+    print("Output (it is 0 by default): ",out)
 else:
     print("Error : Invalid Operator")
