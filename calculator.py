de880fefecfc09438dc717158b826735aff5c215
Akshita <akshitar2003@gmail.com>
2023-08-26 13:41:14 +0530

    solved all issues

--- calculator.py
+++ calculator.py
@@ -1,27 +1,30 @@
 #
-# calculator.py 
+# calculator.py
 #
 
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
-o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+a = int(input("Enter number 1 : "))
+o = input("Enter operator : ")
+b = int(input("Enter number 2 : "))
 
-if o[0] in [ '+','-','*','/' ]:
-    if o[0] == '+':
-        out = a + b
-    elif o[0] == '-':
-        out = a - b
-    elif o[0] == '*':
-        out = a * b
-    elif o[0] == '/':
-        out = a//b
-    elif o[0] == '%':
-        out= a%b
-    elif o[0] == '^':
-        out = a**b
-    print("Output : ",out)
+if o[0] in ['+', '-', '*', '/']:
+  if o[0] == '+':
+    out = a + b
+  elif o[0] == '-':
+    out = a - b
+  elif o[0] == '*':
+    out = a * b
+  elif o[0] == '/':
+    if a / b == 0:
+      print("error divided by zero")
+    else:
+      out = a // b
+  elif o[0] == '%':
+    out = a % b
+  elif o[0] == '^':
+    out = a**b
+  print("Output : ", out)
 else:
-    print("Error : Invalid Operator")
+  print("Error : Invalid Operator")
