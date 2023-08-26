1c1f384a09981434d172fbce4033ab4f240feab2
Sovit Khatri <shovitxettrie819@gmail.com>
2023-08-26 13:37:30 +0530

    Divide by zero error soved and % operator is added

--- calculator.py
+++ calculator.py
@@ -5,9 +5,9 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=int(input("Enter number 2 : "))
 
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
@@ -17,7 +17,17 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if b==0:
+            print("b cannot be zero")
+            exit(0)
+        else:
+            out = a//b
+    elif o[0] == '%':
+        if b==0:
+            print("b cannot be zero")
+            exit(0)
+        else:
+            out = a%b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
