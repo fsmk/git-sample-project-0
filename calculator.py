50dcd4fcd90d0cf75c6464074a68b66d737354bb
Vishalv316 <Vish21ainds@cmrit.ac.in>
2023-08-26 13:34:41 +0530

    changes done

--- calculator.py
+++ calculator.py
@@ -9,15 +9,27 @@ a=input("Enter number 1 : ")
 o=input("Enter operator : ")
 b=input("Enter number 2 : ")
 
-if o[0] in [ '+','-','*','/' ]:
-    if o[0] == '+':
-        out = a + b
-    elif o[0] == '-':
-        out = a - b
-    elif o[0] == '*':
-        out = a * b
-    elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
-else:
-    print("Error : Invalid Operator")
+if isdigit(a) and isdigit(b):
+    if o[0] in [ '+','-','*','/' ,'%','^']:
+        if o[0] == '+':
+            out = a + b
+        elif o[0] == '-':
+            out = a - b
+        elif o[0] == '*':
+            out = a * b
+        elif o[0] == '/':
+            if b==0:
+                print("cannot divide by zero")
+            else:
+                out = a//b
+        elif o[0] == '%':
+            if b==0:
+                print("cannot divide by zero")
+            else:
+                out = a % b
+        elif o[0] == '^':
+            out = pow(a,b)
+    
+        print("Output : ",out)
+    else:
+        print("Error : Invalid Operator")
