ea65806369ff950540bcefc4f9a43cfc207263ef
Jahnavi Undela <astrantia236@gmail.com>
2023-08-26 13:43:49 +0530

    changed code

--- calculator.py
+++ calculator.py
@@ -4,12 +4,12 @@
 
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
-
-a=input("Enter number 1 : ")
+import math
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=int(input("Enter number 2 : "))
 
-if o[0] in [ '+','-','*','/' ]:
+if o[0] in [ '+','-','*','/','%','^']:
     if o[0] == '+':
         out = a + b
     elif o[0] == '-':
@@ -17,7 +17,18 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if(b==0):
+            print("Invalid Input!!")
+        else:
+            out = a//b
+    elif o[0] == '**':
+        out=math.pow(a,b)
+        #out=a**b
+    elif o[0]=='%':
+        if(b==0):
+            print("Invalid Input!!")
+        else:
+            out = a%b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
