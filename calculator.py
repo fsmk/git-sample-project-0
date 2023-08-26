664aac5d7eae98e8f6bd876344e89feb9aefa19c
unknown <lambodarnaik572@gmail.com>
2023-08-26 13:38:10 +0530

    fixed zero error

--- calculator.py
+++ calculator.py
@@ -17,7 +17,10 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if b==0:
+            out="Error"
+        else:
+            out = a/b 
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
