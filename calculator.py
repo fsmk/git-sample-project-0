3354b5aeacb3445a1a13d7a8d8b9df6fc876ec11
Madhav Jaishi <madhavjaishi0@gmail.com>
2023-08-26 13:36:58 +0530

    Fixed -divide by zero error

--- calculator.py
+++ calculator.py
@@ -5,10 +5,10 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
-
+b=int(input("Enter number 2 : "))
+flag = 0
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
         out = a + b
@@ -17,7 +17,12 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
+        if b == 0:
+            print("Divide by zero error")
+            flag = 1
+        else :
+            out = a//b
+    if flag == 0:
+        print("Output : ",out)
 else:
     print("Error : Invalid Operator")
