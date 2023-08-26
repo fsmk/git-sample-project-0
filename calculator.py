8d53566a7cc1fe66836494cdfc9d8c57dcd567a9
Sameer <sameerchandra12@gmail.com>
2023-08-26 13:39:44 +0530

    Resloved divisor issue

--- calculator.py
+++ calculator.py
@@ -5,10 +5,10 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
-
+b=int(input("Enter number 2 : "))
+out =0
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
         out = a + b
@@ -17,7 +17,11 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if b==0:
+            print("Divisor cannot be zero")
+            out = None
+        else:
+            out = a // b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
