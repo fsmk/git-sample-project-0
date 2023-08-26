838e7d2ac3d30a534ba6fa2b732dff2f7d39fb39
Yogesh <ybhatta70@gmail.com>
2023-08-26 13:33:47 +0530

    Zero division error handled and modulus added

--- calculator.py
+++ calculator.py
@@ -5,19 +5,28 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=int(input("Enter number 2 : "))
 
-if o[0] in [ '+','-','*','/' ]:
-    if o[0] == '+':
-        out = a + b
-    elif o[0] == '-':
-        out = a - b
-    elif o[0] == '*':
-        out = a * b
-    elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
-else:
-    print("Error : Invalid Operator")
+try:
+    if o[0] in [ '+','-','*','/','%' ]:
+        if o[0] == '+':
+            out = a + b
+        elif o[0] == '-':
+            out = a - b
+        elif o[0] == '*':
+            out = a * b
+        elif o[0] == '/':
+            if b == 0:
+                raise ZeroDivisionError("Division by Zero Error !!")
+            out = a//b
+        elif o[0] == '%':
+            if b==0:
+                raise ZeroDivisionError("Division by Zero Error !!")
+            out = a % b
+        print("Output : ",out)
+    else:
+        print("Error : Invalid Operator")
+except ZeroDivisionError as z:
+    print(z)
