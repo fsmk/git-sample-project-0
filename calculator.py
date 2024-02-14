f35fdaa1820ba9f87dd19139dde91e294dcd72b8
Dheerays <shaktidheeraysofic@gmail.com>
2024-02-14 12:45:01 +0530

    fixed

--- calculator.py
+++ calculator.py
@@ -1,23 +1,21 @@
-#
-# calculator.py 
-#
+# calculator.py
 
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
-o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+a = float(input("Enter number 1: "))
+o = input("Enter operator: ")
+b = float(input("Enter number 2: "))
 
-if o[0] in [ '+','-','*','/' ]:
-    if o[0] == '+':
+if o in ['+', '-', '*', '/']:
+    if o == '+':
         out = a + b
-    elif o[0] == '-':
+    elif o == '-':
         out = a - b
-    elif o[0] == '*':
+    elif o == '*':
         out = a * b
-    elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
+    elif o == '/':
+        out = a / b
+    print("Output:", out)
 else:
-    print("Error : Invalid Operator")
+    print("Error: Invalid Operator")
