f045b835ea924ff28ffd8ccefb26d4fda526dd4d
Lokesh <lokeshmuruganandham@gmail.com>
2024-02-14 12:43:08 +0530

    Issue Fixed - Not reading types

--- calculator.py
+++ calculator.py
@@ -5,10 +5,12 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
+b=int(input("Enter number 2 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
-
+if 0 == '/' and b == 0:
+    print("Any number cannot be divided with zero")
+    exit
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
         out = a + b
