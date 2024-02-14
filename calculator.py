53d1a3e2cedaddafa712e4a3127f0d17d61a36be
Maria <mariadeeptitt@gmail.com>
2024-02-14 12:42:18 +0530

    issue fixed

--- calculator.py
+++ calculator.py
@@ -5,19 +5,29 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=int(input("Enter number 2 : "))
 
-if o[0] in [ '+','-','*','/' ]:
+
+
+if o[0] in [ '+','-','*','/','%','**' ]:
+
     if o[0] == '+':
         out = a + b
     elif o[0] == '-':
         out = a - b
     elif o[0] == '*':
         out = a * b
+    elif o[0] == '%':
+        out = a % b 
+    elif o[0] == '**':
+        out = a ** b
     elif o[0] == '/':
-        out = a//b
+        if b==0:
+            print("Divide by 0 ERROR")
+        else:
+            out = a/b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
