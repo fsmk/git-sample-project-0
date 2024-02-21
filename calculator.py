6fe3ce1d757a627d8fb131df8dc86cbef679dff3
nawseekhiya <araj80317@gmail.com>
2024-02-21 12:28:48 +0530

    Add type check and division by zero error handling

--- calculator.py
+++ calculator.py
@@ -5,11 +5,12 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
-o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+# Checks the operands type is integer
+a = int(input("Enter number 1: "))
+o = input("Enter operator: ")
+b = int(input("Enter number 2: "))
 
-if o[0] in [ '+','-','*','/' ]:
+if o[0] in ['+', '-', '*', '/']:
     if o[0] == '+':
         out = a + b
     elif o[0] == '-':
@@ -17,7 +18,10 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
+        if b != 0:
+            out = a / b
+        else:
+            out = "Division by Zero Error"  # Handles the division by zero error
+    print("Output: ", out)
 else:
-    print("Error : Invalid Operator")
+    print("Error: Invalid Operator")
