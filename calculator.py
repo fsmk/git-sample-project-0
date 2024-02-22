639b5be7e3e6323a3e35bfe614b536cf6c1b175f
Elonmusk2050 <mailtovinodkn@gmail.com>
2024-02-22 15:31:50 +0530

    Fixed bug 1 and 2

--- calculator.py
+++ calculator.py
@@ -5,19 +5,23 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=int(input("Enter number 2 : "))
 
-if o[0] in [ '+','-','*','/' ]:
-    if o[0] == '+':
+if o in [ '+','-','*','/' ]:
+    if o == '+':
         out = a + b
-    elif o[0] == '-':
+    elif o == '-':
         out = a - b
-    elif o[0] == '*':
+    elif o == '*':
         out = a * b
-    elif o[0] == '/':
-        out = a//b
-    print("Output : ",out)
+    elif o == '/':
+        if b != 0:
+          out = a / b
+    else:
+        print("Error : division by zero")
+        exit()
+    print("Output:", out)
 else:
-    print("Error : Invalid Operator")
+    print("Error: Invalid operator")
