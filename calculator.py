968e5e37f295c170f83db3a8a23551544e7f6fba
julia-p-laiju <julia.p.laiju.6@gmail.com>
2024-02-22 15:20:48 +0530

    Fixed bug 1 and 2

--- calculator.py
+++ calculator.py
@@ -1,13 +1,11 @@
-#
 # calculator.py 
-#
 
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=float(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=float(input("Enter number 2 : "))
 
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
@@ -17,7 +15,11 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if b == 0:
+            print("Divide by zero error")
+            exit()
+        else:
+            out = a//b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
