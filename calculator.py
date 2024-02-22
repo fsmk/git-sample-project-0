668766a411bb0569086e51f63962a97646c39a4c
cemxl <117196230+cemxl@users.noreply.github.com>
2024-02-22 15:23:27 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,19 @@
-#
-# calculator.py 
-#
-
-# Asks user for 2 operands and 1 operator
-# Returns output of this operation
-
-a=input("Enter number 1 : ")
+a=float(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=float(input("Enter number 2 : "))
 
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
-        out = a + b
+        out = a+b
     elif o[0] == '-':
         out = a - b
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if b==0:
+            print("error")
+        else:
+            out = a//b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
